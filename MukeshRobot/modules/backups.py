0333f2fffe2dd1657274445c0aca99cfba111d7e
SHIVANSH475 <158449266+SHIVANSH475@users.noreply.github.com>
2024-02-04 14:51:22 +0530

    Update backups.py

--- MukeshRobot/modules/backups.py
+++ MukeshRobot/modules/backups.py
@@ -341,7 +341,7 @@ def export_data(update, context):
     context.bot.sendDocument(
         current_chat_id,
         document=open("MukeshRobot{}.backup".format(chat_id), "rb"),
-        caption="📤*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `MukeshRobot-Backup` was specially made for notes 📚.".format(
+        caption="📤*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `ShivanshRobot-Backup` was specially made for notes 📚.".format(
             chat.title, chat_id, tgl
         ),
         timeout=360,
