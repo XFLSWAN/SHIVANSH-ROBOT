75fd09375f880a67f621b6841e87f34a25e4e85c
SHIVANSH475 <158449266+SHIVANSH475@users.noreply.github.com>
2024-02-05 10:04:14 +0530

    Update translator.py

--- MukeshRobot/modules/translator.py
+++ MukeshRobot/modules/translator.py
@@ -78,7 +78,7 @@ from telethon import __version__ as s
 from MukeshRobot import OWNER_ID, dispatcher
 from MukeshRobot import pbot as client
 
-Mukesh = "https://te.legra.ph/file/1a72f3770dcb90ee8b3f7.jpg"
+Mukesh = "https://telegra.ph/file/3b3e570ccd647c1dd4798.jpg"
 
 
 @client.on_message(filters.command(["repo", "source"]))
@@ -93,7 +93,7 @@ async def repo(client, message):
 **» ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{s}` 
 **» ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{z}`
 
-**ɢʀᴏᴜᴘ ✘ ᴄᴏɴᴛʀᴏʟʟᴇʀ sᴏᴜʀᴄᴇ ɪs ɴᴏᴡ ᴩᴜʙʟɪᴄ ᴀɴᴅ ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ᴏᴡɴ ʙᴏᴛ.**
+**ᴍᴇʟᴀɴɪᴀ ✘ ᴍᴀɴᴀɢᴇʀ ʙᴏᴛ sᴏᴜʀᴄᴇ ɪs ɴᴏᴛ ᴩᴜʙʟɪᴄ.**
 """,
         reply_markup=InlineKeyboardMarkup(
             [
@@ -102,8 +102,8 @@ async def repo(client, message):
                         "• ᴏᴡɴᴇʀ ",user_id=OWNER_ID
                     ),
                     InlineKeyboardButton(
-                        "• ʀᴇᴘᴏ •",
-                        url="\x68\x74\x74\x70\x73\x3A\x2F\x2F\x67\x69\x74\x68\x75\x62\x2E\x63\x6F\x6D\x2F\x4E\x6F\x6F\x62\x2D\x4D\x75\x6B\x65\x73\x68\x2F\x4D\x75\x6B\x65\x73\x68\x52\x6F\x62\x6F\x74",
+                        "• ᴜᴘᴅᴀᴛᴇs •",
+                        url="https://t.me/SHIVANSH474",
                     ),
                 ]
             ]
